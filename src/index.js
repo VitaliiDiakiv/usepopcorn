db80977d48d78e053e41726717ba47cdbc63970e
Vitalii <vitalii.diakiv.it@gmail.com>
2024-10-05 22:34:23 +0300

    Implement API with useEffect

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App.js";
-import StarRating from "./StarRating.js";
+import "./index.css";
+import App from "./App.js";
+// import StarRating from "./StarRating.js";
+// import App_v1 from "./App_v1.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} messages={["1", "23", "34", "45", "56"]} />
-    <StarRating size={24} color="red" defaultRating={3} />
+    <App />
+    {/* <StarRating maxRating={5} />
+    <StarRating size={24} color="red" defaultRating={3} /> */}
+    {/* <App_v1 /> */}
   </React.StrictMode>
 );
